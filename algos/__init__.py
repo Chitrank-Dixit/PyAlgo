96bafa62c5125096cb0d58ecdf944a977812c0a3
chitrankdixit <chitrank.dixit@flyrobe.com>
2016-06-14 16:13:31 +0530

    first commit

---
+++ algos/__init__.py
@@ -0,0 +1,6 @@
+"""
+	Algos module consits of all the basic algorithms and their implementation
+"""
+
+from .operations import *
+from .structures import *
---
+++ build/lib/algos/__init__.py
@@ -0,0 +1,6 @@
+"""
+	Algos modules consits of all the basic and complex algorithms 
+"""
+
+from .structures import *
+from .operations import *
